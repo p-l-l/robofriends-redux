083bba02136560de0d7ca69fdd5368c67ec1c18f
Pierre-Luc Lemire <p-l-l@hotmail.com>
2020-10-25 20:07:07 -0400

    updated

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { createLogger } from 'redux-logger';
 import 'tachyons';
 
 import App from './containers/App';
-import registerServiceWorker from './registerServiceWorker';
+import * as serviceWorker from './serviceWorker';
 import { requestRobots, searchRobots } from './reducers'
 
 import './index.css';
@@ -24,4 +24,4 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('root')
 );
-registerServiceWorker();
+serviceWorker.register();
